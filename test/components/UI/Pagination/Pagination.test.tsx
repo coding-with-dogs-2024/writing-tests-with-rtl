935165b96c720229ea39d143309e65cee9c118ad
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-26 21:41:04 -0500

    Finished another test

--- test/components/UI/Pagination/Pagination.test.tsx
+++ test/components/UI/Pagination/Pagination.test.tsx
@@ -1,8 +1,9 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Pagination } from '../../../../src/components/UI/Pagination';
-import type {MockedFunction} from 'vitest';
+import type { MockedFunction } from 'vitest';
+import { userEvent } from '@testing-library/user-event';
 
-test('Pagination with the Previous button', () => {
+test('Pagination with the Previous button', async () => {
 	const previousPage: MockedFunction<() => void> = vi.fn();
 	const nextPage: MockedFunction<() => void> = vi.fn();
 	render(
@@ -18,6 +19,28 @@ test('Pagination with the Previous button', () => {
 	expect(buttons[0]).toHaveTextContent('Previous');
 
 	expect(screen.queryByText('Next')).not.toBeInTheDocument();
+
+	await userEvent.click(buttons[0]);
+	expect(previousPage).toHaveBeenCalledOnce();
 });
 
-test.fails('Pagination with the Next button');
+test('Pagination with the Next button', async () => {
+	const previousPage: MockedFunction<() => void> = vi.fn();
+	const nextPage: MockedFunction<() => void> = vi.fn();
+	render(
+		<Pagination
+			showPreviousPage={false}
+			showNextPage={true}
+			previousPage={previousPage}
+			nextPage={nextPage}
+		/>
+	);
+	const buttons = screen.getAllByRole('button');
+	expect(buttons).toHaveLength(1);
+	expect(buttons[0]).toHaveTextContent('Next');
+
+	expect(screen.queryByText('Previous')).not.toBeInTheDocument();
+
+	await userEvent.click(buttons[0]);
+	expect(nextPage).toHaveBeenCalledOnce();
+});
