36a469ed12fa2a9210076ba0ffcf2ea25c5a4700
Coding With Dogs <codingwithdogs@gmail.com>
2023-11-26 19:33:11 -0500

    Progress with tests

--- test/components/UI/Pagination/Pagination.test.tsx
+++ test/components/UI/Pagination/Pagination.test.tsx
@@ -1,3 +1,23 @@
-test.fails('Pagination with the Previous button');
+import {render, screen} from '@testing-library/react';
+import { Pagination } from '../../../../src/components/UI/Pagination';
+import type {MockedFunction} from 'vitest';
+
+test('Pagination with the Previous button', () => {
+	const previousPage: MockedFunction<() => void> = vi.fn();
+	const nextPage: MockedFunction<() => void> = vi.fn();
+	render(
+		<Pagination
+			showPreviousPage={true}
+			showNextPage={false}
+			previousPage={previousPage}
+			nextPage={nextPage}
+		/>
+	);
+	const buttons = screen.getAllByRole('button');
+	expect(buttons).toHaveLength(1);
+	expect(buttons[0]).toHaveTextContent('Previous');
+
+	expect(screen.queryByText('Next')).not.toBeInTheDocument();
+});
 
 test.fails('Pagination with the Next button');
